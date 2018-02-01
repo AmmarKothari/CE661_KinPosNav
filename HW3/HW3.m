adda42e541e04a283bccbbd1ebad09f725b64890
Ammar <amarsbars@gmail.com>
2018-01-31 22:47:07 -0800

    HW3 complete

--- HW3/HW3.m
+++ HW3/HW3.m
@@ -14,17 +14,26 @@ V = zeros(size(M,1)+1,1); % velocity
 P = zeros(size(M,1)+1,1); % position
 
 V(1) = v_0;
+P(1) = x_0;
 for i = 1:size(M,1)
     V(i+1) = V(i) + dt*M(i,1);
-    P(i+1) = P(i) + 1/2*dt*dt*M(i,1);
+    P(i+1) = P(i) + dt*V(i+1) + 1/2*dt*dt*M(i,1);
 end
 
-plot(t, V, 'r-','DisplayName','Velocity');
-hold on;
-plot(t, P, 'b-','DisplayName','Position');
-legend(gca,'show')
-hold off
-title('Position and Velocity from Accelerometer Data')
+Names = {'Position', 'Velocity', 'Acceleration'};
+Data = {P, V, M(:,1)};
+Units = {'m', 'm/s', 'm/s^2'};
+Colors = {'r-', 'b-', 'g-'};
+for i = 1:3
+    figure(i)
+    plot(t(1:length(Data{i})), Data{i}, Colors{i},'DisplayName',Names{i});
+%     legend(gca,'show')
+    title([Names{i}, ' from Accelerometer Data'])
+    xlabel('Time (s)');
+    ylabel(sprintf('%s (%s)', Names{i}, Units{i}));
+    saveas(gcf, strcat(Names{i},'.png'));
+    close(gcf);
+end
 
 %% Unbounded error
 bias = 70*10^-6;
@@ -33,15 +42,18 @@ P_error = zeros(size(M,1)+1,1); % position
 
 for i = 1:size(M,1)
     V_error(i+1) = V_error(i) + dt*bias;
-    P_error(i+1) = P_error(i) + 1/2*dt*dt*bias;
+    P_error(i+1) = P_error(i) + dt*V_error(i) + 1/2*dt*dt*bias;
 end
-
-plot(t, V, 'r-','DisplayName','Velocity Error');
-hold on;
-plot(t, P, 'b-','DisplayName','Position Error');
-legend(gca,'show')
-hold off
-title('Error in Position and Velocity from Accelerometer Bias');
+Data = {P_error, V_error, M(:,1)};
+for i = 1:2
+    figure(i)
+    plot(t(1:length(Data{i})), Data{i}, Colors{i},'DisplayName',Names{i});
+    title(['Error in ', Names{i}, ' from Accelerometer Bias'])
+    xlabel('Time (s)');
+    ylabel(sprintf('%s Error (%s)', Names{i}, Units{i}));
+    saveas(gcf, strcat(Names{i},'_error.png'));
+    close(gcf);
+end
 
 %% Down Direction
 down = mean(M(:,3));
