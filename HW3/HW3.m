ebaf532e3008d8d106e309d3c6c181dc7a41438e
Ammar <amarsbars@gmail.com>
2018-02-01 09:59:45 -0800

    testing out total error in displacement

--- HW3/HW3.m
+++ HW3/HW3.m
@@ -1,6 +1,9 @@
 fname = 'xnav_inertial_meas.csv';
 
 M = csvread(fname, 1,0);
+Names = {'Position', 'Velocity', 'Acceleration'};
+Units = {'m', 'm/s', 'm/s^2'};
+Colors = {'r-', 'b-', 'g-'};
 %%
 sampling_rate = 100; %hz
 t_0 = 0;
@@ -20,10 +23,7 @@ for i = 1:size(M,1)
     P(i+1) = P(i) + dt*V(i+1) + 1/2*dt*dt*M(i,1);
 end
 
-Names = {'Position', 'Velocity', 'Acceleration'};
 Data = {P, V, M(:,1)};
-Units = {'m', 'm/s', 'm/s^2'};
-Colors = {'r-', 'b-', 'g-'};
 for i = 1:3
     figure(i)
     plot(t(1:length(Data{i})), Data{i}, Colors{i},'DisplayName',Names{i});
@@ -39,7 +39,6 @@ end
 bias = 70*10^-6;
 V_error = zeros(size(M,1)+1,1); % velocity
 P_error = zeros(size(M,1)+1,1); % position
-
 for i = 1:size(M,1)
     V_error(i+1) = V_error(i) + dt*bias;
     P_error(i+1) = P_error(i) + dt*V_error(i) + 1/2*dt*dt*bias;
@@ -54,6 +53,35 @@ for i = 1:2
     saveas(gcf, strcat(Names{i},'_error.png'));
     close(gcf);
 end
+%% Overall travel error
+V = zeros(size(M,1)+1,2); % velocity
+P = zeros(size(M,1)+1,2); % position
+
+V(1,:) = v_0;
+P(1,:) = x_0;
+for j = 1:2
+    signed_bias = (-1)^(j-1) * bias; 
+    for i = 1:size(M,1)
+        V(i+1,j) = V(i,j) + dt*(M(i,1) + signed_bias);
+        P(i+1,j) = P(i,j) + dt*V(i+1,j) + 1/2*dt*dt*(M(i,1) + signed_bias);
+    end
+end
+
+Data = {P, V, M(:,1)};
+for i = 1:2
+    figure(i)
+    plot(t(1:length(Data{i})), Data{i}(:,1), Colors{i},'DisplayName',Names{i});
+    hold on
+    plot(t(1:length(Data{i})), Data{i}(:,2), Colors{i},'DisplayName',Names{i});
+    hold off
+%     legend(gca,'show')
+    title([Names{i}, ' from Accelerometer Data'])
+    xlabel('Time (s)');
+    ylabel(sprintf('%s (%s)', Names{i}, Units{i}));
+    saveas(gcf, strcat(Names{i},'_bias.png'));
+%     close(gcf);
+end
+
 
 %% Down Direction
 down = mean(M(:,3));
