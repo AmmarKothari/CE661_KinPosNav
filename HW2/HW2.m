58029985d1c310e990417cf2f3ce5b399dae167d
Ammar Kothari <ammar.kothari@gmail.com>
2018-02-11 13:31:34 -0800

    Finished hw2 and started hw4

--- HW2/HW2.m
+++ HW2/HW2.m
@@ -13,3 +13,29 @@ psi = 90.084;
 [q1, q2, q3, q4] = TaitByran2quaternion(phi, theta, psi);
 quat = [q1, q2, q3, q4]
 C_NED_IMU = TaitBryan2DCM(phi, theta, psi)
+
+%% 4
+DCM = TaitBryan2DCM(-3.222, -9.316, 96.061)
+
+%% 5
+p1 = [5.7; 2.5];
+theta = deg2rad(-30);
+R_c = [cos(theta), -sin(theta); sin(theta), cos(theta)];
+theta_p = -theta;
+R = [cos(theta_p), -sin(theta_p); sin(theta_p), cos(theta_p)];
+p2 = R * p1
+
+
+x1 = [0, 10; 0, 0];
+y1 = [0, 0; 0, 10];
+x2 = R_c * x1;
+y2 = R_c * y1;
+plot(x1(1,:), x1(2,:), 'rx-')
+hold on;
+plot(y1(1,:), y1(2,:), 'rx-')
+plot(x2(1,:), x2(2,:), 'bx:')
+plot(y2(1,:), y2(2,:), 'bx:')
+plot(p1(1), p1(2), 'ro');
+plot(p2(1), p2(2), 'bo');
+hold off;
+axis equal
