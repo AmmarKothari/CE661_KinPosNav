1112a1c069223274aadec1507dedd8fb413dc0ce
Ammar <amarsbars@gmail.com>
2018-02-27 21:55:27 -0800

    updating some settings files

--- HW5/get_files.sh
+++ HW5/get_files.sh
@@ -1,2 +1,4 @@
 wget ftp://ftp.engr.oregonstate.edu/pub/civil/Parrish/UAS_SfM_workshop/GCPs_tower.txt
+mkdir images
+cd images
 wget ftp://ftp.engr.oregonstate.edu/pub/civil/Parrish/UAS_SfM_workshop/Sample_imagery/*.jpg
